a9449b19bdec4f0dfdfb8e7a12e8282b0dfa035a
Bipin Khanal <bipin.khanal1050@gmail.com>
2023-05-05 20:11:56 +0545

    initializing

---
+++ src/welcome/welcome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <div className="font-bold text-center bg-black text-white h-12 flex flex-col al">
+        Let's begin the play
+    </div>
+  )
+}
+
+export default Welcome
